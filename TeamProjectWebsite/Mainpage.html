8fd897081499a2baf92a144b627e94a87aac1f5a
AkashdeepSingh07 <165279898+AkashdeepSingh07@users.noreply.github.com>
2024-04-26 23:35:37 +0930

    26/04/24: Login,logout google

---
+++ TeamProjectWebsite/Mainpage.html
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>SACE Student Portal</title>
+<link rel="stylesheet" href="Mainpage.css">
+</head>
+<body>
+ 
+  <header class="main-header">
+    <div class="logo-container">
+      <img class="header-title" src="Eximages/Sace_logo.png" alt="SACE Portal Logo">
+      <span class="header-title">SACE Portal</span>
+    </div>
+    <div class="nav-container">
+    <nav class="main-nav">
+      <a href="#">Home</a><a href="#">Courses</a> <a href="#">Services</a><a href="#">Contact</a><a href="#">Help</a>
+      </nav>
+      <div class="search-container">
+        <input type="search" placeholder="Search">
+        <button type="submit"><img src="search.png" alt="Search"></button>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <section class="courses-grid">
+      <!-- Course Card 1 -->
+      <article class="course-card">
+        <header class="course-header background-blue">
+          <h2>SACE Japanese (Background)</h2>
+          <button>...</button>
+        </header>
+        <div class="course-body">
+          <p>Stage 1</p>
+          <div class="progress-bar" data-completed="25"></div>
+        </div>
+      </article>
+      <!-- Course Card 2-->
+      <article class="course-card">
+        <header class="course-header background-blue">
+          <h2>SACE Japanese (Background)</h2>
+          <button>...</button>
+        </header>
+        <div class="course-body">
+          <p>Stage 2</p>
+          <div class="progress-bar" data-completed="25"></div>
+        </div>
+      </article>
+      <!-- Course Card 3 -->
+      <article class="course-card">
+        <header class="course-header background-blue">
+          <h2>SACE Japanese (Continuers)</h2>
+          <button>...</button>
+        </header>
+        <div class="course-body">
+          <p>Stage 1</p>
+          <div class="progress-bar" data-completed="25"></div>
+        </div>
+      </article>
+      <!-- Course Card 4 -->
+      <article class="course-card">
+        <header class="course-header background-blue">
+          <h2>SACE Japanese (Continuers)</h2>
+          <button>...</button>
+        </header>
+        <div class="course-body">
+          <p>Stage 2</p>
+          <div class="progress-bar" data-completed="25"></div>
+        </div>
+      </article> 
+    </section>
+  </main>
+
+  <footer class="main-footer">
+    <div class="footer-content">
+      <div class="quick-links">
+        <h3>Quick Links</h3>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Courses</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contacts</a></li>
+          <li><a href="#">Help</a></li>
+        </ul>
+      </div>
+      <div class="contact-us">
+        <h3>Contact Us</h3>
+        <ul>
+          <li><a href="#">Instagram</a></li>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Youtube</a></li>
+        </ul>
+      </div>
+      <div class="address">
+        <h3>Address</h3>
+        <p>Level 5/118 King William St<br>Adelaide, SA<br>Phone: (08) 5555 5555</p>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <img src="Sace_logo.png" alt="SACE Portal Logo">
+      <p>Copyright &copy; Sace Student Portal</p>
+    </div>
+  </footer>
+
+<script src="scripts.js"></script>
+</body>
+</html>
---
+++ untitled folder/experiment2.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="experiment2.css">
+    <link rel="stylesheet" href="colors.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+<body>
+
+<?php
+require_once 'vendor/autoload.php';
+
+// init configuration
+$clientID = '704453595817-2qrd8v8c2rhgl75qvv8iumu11864mo22.apps.googleusercontent.com';
+$clientSecret = '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';
+$redirectUri = 'http://localhost/DraftWebsite/Mainpage.html';
+
+// create Client Request to access Google API
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
+$googleLoginUrl = $client->createAuthUrl();
+
+// authenticate code from Google OAuth Flow
+if (isset($_GET['code'])) {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  $client->setAccessToken($token['access_token']);
+
+  // get profile info
+  $google_oauth = new Google_Service_Oauth2($client);
+  $google_account_info = $google_oauth->userinfo->get();
+  $email =  $google_account_info->email;
+  $name =  $google_account_info->name;
+
+  // now you can use this profile info to create account in your website and make user logged in.
+} else {
+?>
+
+    <div class="container" id="Login">
+       <div class="toplogin"><img src="Eximages/Sace_logo.png" alt="SACE Logo">
+        <h1 id="verticleline"></h1>
+        <h1 id="sacename"><i>SACE Portal</i></h1>
+    </div>
+    <hr id="horizontalline">
+        
+       
+         <h1 id="logintitle">Login</h1>
+
+         <form action="" method="post">
+        <div class="forumtext">
+            <i class="fas fa-envelope"></i>
+            <input type="username" id="username" name="username" placeholder="USERNAME" required> 
+        </div>
+        <div class="forumtext">
+            <i class="fas fa-lock"></i>
+            <input type="password" id="password" name="password" placeholder="PASSWORD" required>
+            <i class="fas fa-sign-in"></i>  
+            <input type="submit" id="submitbtn" name="login" value="Log in">
+            <p id="passwordlost">
+                <a href="#">Lost Password?</a>
+            </p>
+            <p class="or">---------OR----------</p>
+         <div class="googleicon">
+            <a href="<?php echo $client->createAuthUrl() ?>"><i class="fab fa-google">  
+            </i><b><p id="googlelink">Login With Google </p></b> </a>  
+        </div> 
+        </div>
+        <?php } ?>
+         
+    </form>
+
+
+    
+</div>
+<!--<div id="googlebtn">
+            <input type="submit" id="googlebtn" name="googlebutton" value="Sign up with google">
+            <i class="fab fa-google"></i>
+        </div>-->
+         <!--
+             <div class="container" id="Login">
+        <h1 id="verticleline"></h1>
+        <h1>SACE Portal</h1>
+         <h1 id="logintitle">Login</h1>
+         <form action="" method="post">
+            
+            
+            <form action="" method="post">
+           <div class="nametext">
+                <label for="firstname">First Name:</label>
+                <input type="text" id="firstname" name="firstname" placeholder="Ichiro" required> 
+                <i class="fas fa-user"></i>  
+            </div>
+            <div class="forumtext">
+                <label for="lastname">Last Name:</label>
+                <input type="text" id="lastname" name="lastname" placeholder="Suzuki" required> 
+                <i class="fas fa-user"></i>
+            </div>
+            <div class="forumtext">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="IchiroSuzuki.gmail.com" required> 
+                <i class="fas fa-envelope"></i>
+            </div>
+            <div class="forumtext">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" placeholder="************" required> 
+                <i class="fas fa-lock"></i>
+            </div>
+
+            <div class="submitbtn">
+                <input type="submit" id="button" name="signup" value="Sign up">
+            </div>
+            <p id="horizontalline"></p>
+
+           <div id="googlebtn">
+                <input type="submit" id="googlebtn" name="googlebutton" value="Sign up with google">
+                <i class="fab fa-google"></i>
+            </div>
+            <div class="Alreadyacc">
+
+                <p>Aklready have an account</p>
+                <button id="signinbtn">Sign In</button>
+                </div>
+            </forum>
+         </div>-->
+</body>
+</html>
